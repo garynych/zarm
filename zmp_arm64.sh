7483ace8ff96f0eb444aa74805042ac2215d1d1e
sybdata <as15@wt.de>
2020-10-12 13:45:03 +0200

    Update zmp_arm64.sh

--- zmp_arm64.sh
+++ zmp_arm64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /opt && wget https://github.com/sybdata/zarm/blob/master/ZMP/2.1.26/zmp-linux-arm64?raw=true
-chmod +x zmp-linux-amd64
+chmod +x zmp-linux-arm64
 curl -sSL https://raw.githubusercontent.com/sybdata/zarm/master/zmp.service > /etc/systemd/system/zmp.service
 systemctl daemon-reload
 systemctl enable ZMediaProxy
