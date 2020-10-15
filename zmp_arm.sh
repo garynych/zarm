85265d8477bd815c333cb7a1b03ca29008373515
sybdata <as15@wt.de>
2020-10-15 18:57:28 +0200

    Update zmp_arm.sh

--- zmp_arm.sh
+++ zmp_arm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /opt && wget https://github.com/sybdata/zarm/blob/master/ZMP/2.1.26/zmp-linux-arm7?raw=true
+cd /opt && wget https://github.com/sybdata/zarm/blob/master/ZMP/2.1.26/zmp-linux-arm7
 chmod +x zmp-linux-arm7
 curl -sSL https://raw.githubusercontent.com/sybdata/zarm/master/zmp7.service > /etc/systemd/system/zmp7.service
 systemctl daemon-reload
