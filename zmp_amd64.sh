648a085ed59abf8da8341720829394bd570c9568
sybdata <as15@wt.de>
2020-10-15 18:57:48 +0200

    Update zmp_amd64.sh

--- zmp_amd64.sh
+++ zmp_amd64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /opt && wget https://github.com/sybdata/zarm/blob/master/ZMP/2.1.26/zmp-linux-amd64?raw=true
+cd /opt && wget https://github.com/sybdata/zarm/blob/master/ZMP/2.1.26/zmp-linux-amd64
 chmod +x zmp-linux-amd64
 curl -sSL https://raw.githubusercontent.com/sybdata/zarm/master/zmp64.service > /etc/systemd/system/zmp64.service
 systemctl daemon-reload
